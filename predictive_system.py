dc7f4267fcfe24f159967023e14544b82a120953
aryaananya03 <144282697+aryaananya03@users.noreply.github.com>
2023-09-07 02:19:08 +0530

    Update predictive_system.py

--- predictive_system.py
+++ predictive_system.py
@@ -6,6 +6,15 @@ This is a temporary script file."""
 import pickle 
 import numpy as np
 load_model=pickle.load(open("https://github.com/aryaananya03/migraine/blob/main/migraine.sav","rb"))
+response = requests.get(github_raw_url)
+
+
+if response.status_code == 200:
+    
+    load_model = pickle.loads(response.content)
+else:
+    # Handle the case where the request was not successful
+    print("Failed to fetch the model file from GitHub.")
 
 
 input_data_numpy=np.asarray(input_data)
